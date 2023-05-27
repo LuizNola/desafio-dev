c05d9a4fce8c0b80e41a8c5ffc5003d61045892f
Luiz Nola <luizn647@gmail.com>
2023-05-26 22:56:13 -0300

    feat: docker network

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsModule } from './transactions/transactions.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CON || 'postgres',
+      host: process.env.TYPEORM_HOST || 'localhost',
+      port: 5432,
+      username: process.env.TYPEORM_USERNAME || 'postgress',
+      password: process.env.TYPEORM_PASSWORD || 'postgress',
+      database: process.env.TYPEORM_DATABASE || 'bycoders',
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
